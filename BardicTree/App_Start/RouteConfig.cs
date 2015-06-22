4080938bbe3ac70a09d22de34fc535fe6adbf698
Paul Gilbertson <paul.gilbertson@gmail.com>
2015-06-22 18:25:56 +0100

    Route fix for User Profiles

--- BardicTree/App_Start/RouteConfig.cs
+++ BardicTree/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace BardicTree
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "User",
+                url: "{controller}/{id}",
+                defaults: new { controller = "User", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
